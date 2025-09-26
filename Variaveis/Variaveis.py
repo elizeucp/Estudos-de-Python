234375e055f039addb4aa52b4de9ad75f2c7bc3a
elizeucp <elizeucp01@gmail.com>
2025-09-26 19:33:35 -0300

    Add Variaveis Folder

---
+++ Variaveis/Variaveis.py
@@ -0,0 +1,28 @@
+# Variáveis
+
+# Exemplo 1
+velocidade_movel = 400
+
+print(velocidade_movel)
+
+# Tipos de Variáveis
+
+## === Números Inteiros (int) ===
+idade = 5
+
+## === Números Decimais (float) ===
+nota = 7.5
+
+## === Textos (String) ===
+nome_completo = "Elizeu Corrêa" 
+# Pode-se Utilizar Aspas Simples ('xyz') ou Aspas Duplas("xyz"). Não faz Diferença no Python
+
+## === Booleanos (True ou False) ===
+pode_entrar = True
+# Deve-se Escrever em PascalCase (T e F maiúsculo).
+
+
+# Como Descobrir o Tipo de uma Variável?
+
+# Função type()
+print(type(nota))
